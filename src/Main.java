6c3ca9e06cd9e631835331220d767c9b1c2b5586
zubiri <zubiri@Ubuntu-12-04.(none)>
2015-02-03 08:36:26 +0100

    firt version

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+class Main {
+	public static void main(String[] args) {
+		System.out.println("--Estos son los datos del trabajador:--");
+		System.out.println("DNI: " + Trabajador.DNI + "\nNombre: " + Trabajador.nombre + "\nFecha de nacimiento: " + Trabajador.fechaNaci + "\nPuesto: " + Trabajador.puesto);
+	}
+}
---
+++ src/Trabajador.java
@@ -0,0 +1,6 @@
+class Trabajador {
+	static final String DNI = "75418634-S";
+	static final String fechaNaci = "09/05/1990";
+	static String nombre = "Pablo";
+	static String puesto = "Repartidor";
+}
